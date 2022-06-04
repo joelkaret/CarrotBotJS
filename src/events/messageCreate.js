5b0b055a8c6852f22ad7c87ac2b06278b01091d9
Joel Karet <joelkare@gmail.com>
2022-06-04 15:59:18 +0100

    remove random console.log

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -12,7 +12,6 @@ module.exports = {
 		let channelName = `${message.channel.name}_${message.guild.name}`
 		channelName = channelName.split(' ').join('-').toLowerCase()
 		let channel = loggerGuild.channels.cache.find(C => C.name == channelName)
-		console.log(loggerGuild.channels.cache.find(channel => channel.name == channelName))
 		if (!channel) {
 			channel = await loggerGuild.channels.create(channelName)
 		}
