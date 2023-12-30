9e531540f42d7455d93c31e75e92f047d2d3392c
joelkaret <81484847+joelkaret@users.noreply.github.com>
2023-12-30 18:40:51 +0000

    add walter wednesday

--- src/bot.js
+++ src/bot.js
@@ -72,6 +72,27 @@ try {
 } catch {
 	exec("kill 1")
 }
+
+// Walter Wednesday Skillless
+const schedule = require('node-schedule');
+const rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = 3;
+rule.hour = 12;
+rule.minute = 0;
+rule.tz = 'America/Atikokan' // EST
+
+const job = schedule.scheduleJob(rule, function(){
+	const skilllessGuildId = "713646548436910116";
+	const channelId = "759265396901150761"
+	let channel = skilllessGuildId.channels.cache.find(
+                (C) => C.id == channelId
+            );
+	if (channel) {
+		channel.send('https://cdn.discordapp.com/attachments/852716030601330740/1190718826766143520/walter_wedsendy.mp4?ex=65a2d252&is=65905d52&hm=ad65957d76fec08992c35123391585335b9e8daeebad755cd475faa79273e9bb&');
+	}
+});
+
+
 // exec("kill 1")
 
 //client.login(process.env.token)
