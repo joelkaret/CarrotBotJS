f175eede5cdded7d26832b3d99625f523f87af28
Joel Karet <joelkaret@gmail.com>
2024-01-29 21:50:57 +0000

    missing commit again

---
+++ src/functions/handleSchedules.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const { cyanBright, gray } = require("colorette");
+
+module.exports = (client) => {
+	client.handleSchedules = async () => {
+		const scheduleFolders = fs.readdirSync("./src/schedules");
+		for (const folder of scheduleFolders) {
+			const scheduleFiles = fs
+				.readdirSync(`./src/schedules/${folder}`)
+				.filter((file) => file.endsWith(".js"));
+			for (const file of scheduleFiles) {
+				const schedule = require(`../schedules/${folder}/${file}`);
+				try {
+					schedule();
+					console.log(
+						`[${cyanBright("DEBUG")}] ${gray(
+							"Schedule deployed successfully"
+						)}\n`
+					);
+				} catch (error) {
+					console.log(`[${cyanBright("DEBUG")}] ${gray(error)}\n`);
+				}
+			}
+		}
+	};
+};
