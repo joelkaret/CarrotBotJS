c34b0e8ccda5494efcb87a97aa610f59a23d262b
Joel Karet <joelkaret@gmail.com>
2024-02-17 02:14:20 +0000

    add error catch

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,47 +1,48 @@
 const { CommandInteraction } = require("discord.js");
 
 module.exports = {
-	name: 'interactionCreate',
+	name: "interactionCreate",
 	once: false,
 	async execute(interaction, client) {
+		try {
+			if (interaction.isCommand()) {
+				const command = interaction.client.commands.get(
+					interaction.commandName
+				);
 
-		if (interaction.isCommand()) {
+				if (!command) return;
 
-			const command = interaction.client.commands.get(interaction.commandName);
+				try {
+					await command.execute(interaction, client);
+				} catch (err) {
+					if (err) console.error(err);
+					await interaction.reply({
+						content: "An error has occured with this command.",
+						ephermeral: true,
+					});
+				}
+			} else if (interaction.isStringSelectMenu()) {
+				if (interaction.customId.includes("-commandName")) {
+					pass;
+					// Code here for select menu
+				}
+			} else if (interaction.isButton()) {
+				const button = client.buttons.get(interaction.customId);
 
-			if (!command) return;
+				if (!button) return;
 
-			try{
-				await command.execute(interaction, client);
-			} catch (err) {
-				if (err) console.error(err);
-				await interaction.reply({
-					content: "An error has occured with this command.",
-					ephermeral: true,
-				});
+				try {
+					await button.execute(interaction);
+				} catch (err) {
+					if (err) console.error(err);
+					await interaction.reply({
+						content: "An error has occured with this button.",
+						ephermeral: true,
+					});
+				}
 			}
-
-		} else if (interaction.isStringSelectMenu()) {
-			if (interaction.customId.includes('-commandName')) {
-				pass
-				// Code here for select menu
-			}
-
-		} else if (interaction.isButton()) {
-
-			const button = client.buttons.get(interaction.customId);
-
-			if (!button) return;
-
-			try{
-				await button.execute(interaction);
-			} catch (err) {
-				if (err) console.error(err);
-				await interaction.reply({
-					content: "An error has occured with this button.",
-					ephermeral: true,
-				});
-			}
+		} catch (err) {
+			if (err) console.error(err);
 		}
 	},
 };
