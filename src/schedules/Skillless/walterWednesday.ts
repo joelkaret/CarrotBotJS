44992b07f84426bede5ec4bd41d3ba5934ad2908
Joel Karet <joelkaret@gmail.com>
2026-03-06 21:15:19 +0000

    walter will randomly ping someone in the channel

--- src/schedules/Skillless/walterWednesday.ts
+++ src/schedules/Skillless/walterWednesday.ts
@@ -11,15 +11,35 @@ rule.minute = config.walterWednesday.rule.minute;
 rule.tz = config.walterWednesday.rule.tz;
 
 export default (client: Client) => {
-	schedule.scheduleJob(rule, function () {
+	schedule.scheduleJob(rule, async function () {
 		const guild = client.guilds.cache.get(guildId);
 		const channel = guild?.channels.cache.find((C) => C.id == channelId) as
 			| TextChannel
 			| undefined;
-		if (channel) {
-			void channel.send(
-				`It's Walter Wednesday! Enjoy this video: ${config.walterWednesday.url}`
-			);
+
+		if (channel && guild) {
+			// Fetch all members to ensure cache is up to date
+			await guild.members.fetch();
+
+			// Get all members who can view the channel
+			const members = guild.members.cache.filter((member) => {
+				const permissions = channel.permissionsFor(member);
+				return permissions?.has("ViewChannel") ?? false;
+			});
+
+			// Pick a random member
+			const randomMember = members.random();
+
+			if (randomMember) {
+				void channel.send(
+					`It's Walter Wednesday! ${randomMember.toString()}, enjoy this video: ${config.walterWednesday.url}`
+				);
+			} else {
+				// Fallback if no members found (unlikely)
+				void channel.send(
+					`It's Walter Wednesday! Enjoy this video: ${config.walterWednesday.url}`
+				);
+			}
 		}
 	});
 };
