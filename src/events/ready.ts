19300a59cbbed2b8a9bd25af307e80fa9eab4e27
Joel Karet <joelkaret@gmail.com>
2026-03-17 15:30:33 +0000

    fix deprecation warning: ready->clientReady

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,7 +2,7 @@ import log from "../utils/logger";
 import type { Client } from "discord.js";
 
 export default {
-	name: "ready",
+	name: "clientReady",
 	once: true,
 	execute(client: Client) {
 		if (!client.user) return;
